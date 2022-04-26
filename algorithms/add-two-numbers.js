7a467eaaf14a4e363bf260b1467d07658d7004cc
w2xi <43wangxi@gmail.com>
2022-04-26 20:56:19 +0800

    Add Two Numbers (the best solution)

--- algorithms/add-two-numbers.js
+++ algorithms/add-two-numbers.js
@@ -5,6 +5,57 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+
+
+// ----- 方法一(最佳) ------- //
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  let head, tail
+  // 表示进位
+  let carry = 0
+
+  while (l1 || l2) {
+    const n1 = l1 ? l1.val : 0
+    const n2 = l2 ? l2.val : 0
+    const sum = n1 + n2 + carry
+
+    carry = Math.floor(sum / 10)
+
+    // 当前数值
+    const curr = sum % 10
+
+    if (!head) {
+      head = tail = new ListNode(curr)
+    } else {
+      tail.next = new ListNode(curr)
+      tail = tail.next
+    }
+
+    if (l1) {
+      l1 = l1.next
+    }
+    if (l2) {
+      l2 = l2.next
+    }
+  }
+
+  // 如果最后的进位大于0
+  if (carry > 0) {
+    tail.next = new ListNode(carry)
+  }
+
+  return head
+};
+
+
+
+// ----- 方法二 (BigInt取巧) ------- //
+
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
