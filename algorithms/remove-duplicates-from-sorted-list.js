423917c32070a087d3efb252f13625d148e69403
w2xi <43wangxi@gmail.com>
2022-05-17 22:12:32 +0800

    style: prettier code

--- algorithms/remove-duplicates-from-sorted-list.js
+++ algorithms/remove-duplicates-from-sorted-list.js
@@ -10,15 +10,15 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
- var deleteDuplicates = function(head) {
+var deleteDuplicates = function (head) {
   let curr = head;
 
   while (curr && curr.next) {
-      if (curr.val === curr.next.val) {
-          curr.next = curr.next.next;
-      } else {
-          curr = curr.next;
-      }
+    if (curr.val === curr.next.val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
   }
 
   return head;
