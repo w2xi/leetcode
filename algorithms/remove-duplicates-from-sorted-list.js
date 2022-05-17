30e8e246ba6e5f965fbfd636d20611df783206b6
w2xi <43wangxi@gmail.com>
2022-05-17 21:15:13 +0800

    Remove Duplicates from Sorted List

---
+++ algorithms/remove-duplicates-from-sorted-list.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * 删除排序链表中的重复元素
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var deleteDuplicates = function(head) {
+  let curr = head;
+
+  while (curr && curr.next) {
+      if (curr.val === curr.next.val) {
+          curr.next = curr.next.next;
+      } else {
+          curr = curr.next;
+      }
+  }
+
+  return head;
+};
