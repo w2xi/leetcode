b445b698f7ab1a1ce31d9b532546407ffebf51cf
w2xi <43wangxi@gmail.com>
2022-05-21 19:51:39 +0800

    Reverse Linked List (rescursive)

--- algorithms/reverse-linked-list.js
+++ algorithms/reverse-linked-list.js
@@ -1,5 +1,3 @@
-// 常规解法
-
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -8,44 +6,38 @@
  * }
  */
 /**
+ * 反转链表
  * @param {ListNode} head
  * @return {ListNode}
+ * @example
+ * 
+ * Input: 1->2->3->4->5
+ * Output: 5->4->3->2->1
  */
 var reverseList = function (head) {
-  let hNode;
-  
-  while (head) {
-    hNode = new ListNode(head.val, hNode);
+  // 1. 递归
+  const reverseNode = (curr, prev) => {
+    if (curr) {
+      const next = curr.next;
+      curr.next = prev;
+      return reverseNode(next, curr);
+    } else {
+      return prev;
+    }
+  };
+  return reverseNode(head, null);
 
-    head = head.next;
-  }
+  // 2. 迭代
 
-  return hNode || head;
-};
+  // let prev = null;
+  // let curr = head;
 
-// 迭代法
+  // while (curr) {
+  //   let next = curr.next;
+  //   curr.next = prev;
+  //   prev = curr;
+  //   curr = next;
+  // }
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
- var reverseList = function(head) {
-  let prev = null;
-  let curr = head
-
-  while (curr) {
-      let next = curr.next
-      curr.next = prev
-      prev = curr
-      curr = next
-  }
-
-  return prev
+  // return prev;
 };
