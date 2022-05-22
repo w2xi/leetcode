927df37eece1772a995e045b163b469e5b6b3a6d
w2xi <43wangxi@gmail.com>
2022-05-22 10:50:58 +0800

    Middle of the Linked List

---
+++ algorithms/middle-of-the-linked-list.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * 链表的中间结点 (快慢指针)
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+};
