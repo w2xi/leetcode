330bd191e3c2cd78be26920f87bc81d7ef99d1a0
w2xi <43wangxi@gmail.com>
2022-06-05 23:40:01 +0800

    update comments

--- algorithms/maximum-subarray.js
+++ algorithms/maximum-subarray.js
@@ -4,6 +4,8 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
+  // 数据结构 (浙江大学)
+  // 参考: https://www.icourse163.org/learn/ZJU-93001?tid=1462787444#/learn/content?type=detail&id=1239897398&cid=1261105609
   let thisSum = 0,
     maxSum = nums[0];
 
