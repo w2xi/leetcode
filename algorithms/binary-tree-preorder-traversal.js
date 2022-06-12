8710b7a268b4060eb7a900fadddc6ccb42760d3e
w2xi <43wangxi@gmail.com>
2022-06-12 09:41:19 +0800

    Binary Tree Preorder Traversal (迭代法)

--- algorithms/binary-tree-preorder-traversal.js
+++ algorithms/binary-tree-preorder-traversal.js
@@ -7,11 +7,12 @@
  * }
  */
 /**
- * 二叉树的前序遍历 (以优先于后代节点的顺序访问每个节点的)
+ * 二叉树的前序遍历
  * @param {TreeNode} root
  * @return {number[]}
  */
 var preorderTraversal = function (root) {
+  // 1. 递归
   const arr = [];
   const preOrderTravelsalNode = (node) => {
     if (node) {
@@ -23,4 +24,24 @@ var preorderTraversal = function (root) {
   preOrderTravelsalNode(root);
 
   return arr;
+
+  // 2. 迭代
+  // return preOrderTraversalIterate(root);
+}
+
+function preOrderTraversalIterate(root){
+  const result = [];
+  const stack = [];
+  root && stack.push(root);
+
+  while (stack.length > 0) {
+    // 出栈
+    const node = stack.pop();
+    result.push(node.val);
+    // 入栈
+    node.right && stack.push(node.right);
+    node.left && stack.push(node.left);
+  }
+
+  return result;
 };
