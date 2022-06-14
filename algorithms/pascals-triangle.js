051fef67ab26c552dadacf26a24a4d5bc3debbad
w2xi <43wangxi@gmail.com>
2022-06-14 22:36:51 +0800

    118. 杨辉三角

---
+++ algorithms/pascals-triangle.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+  const result = Array.from({ length: numRows }, (_, val) =>
+    Array(val + 1).fill(1)
+  );
+
+  for (let i = 0; i < result.length; i++) {
+    const row = result[i];
+    if (i > 1) {
+      const prevRow = result[i - 1];
+      for (let j = 1; j < row.length - 1; j++) {
+        row[j] = prevRow[j - 1] + prevRow[j];
+      }
+    }
+  }
+
+  return result;
+};
