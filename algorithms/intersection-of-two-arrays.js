74e2c9e71ac97de3779610181c66a15d2b5d6c54
w2xi <43wangxi@gmail.com>
2022-06-24 19:53:50 +0800

    349. 两个数组的交集

--- algorithms/intersection-of-two-arrays.js
+++ algorithms/intersection-of-two-arrays.js
@@ -4,7 +4,21 @@
  * @return {number[]}
  */
 
-// 方法一：哈希表（降低时间复杂度）
+// 方法一: 集合
+var intersection = function (nums1, nums2) {
+  const s = new Set(nums1);
+  const resultSet = new Set();
+
+  for (let item of nums2) {
+    if (s.has(item)) {
+      resultSet.add(item);
+    }
+  }
+
+  return Array.from(resultSet);
+};
+
+// 方法二：哈希表（降低时间复杂度）
 var intersection = function (nums1, nums2) {
   const result = [];
   const nums2Obj = {};
@@ -14,13 +28,10 @@ var intersection = function (nums1, nums2) {
   for (let i = 0; i < nums2.length; i++) {
     nums2Obj[nums2[i]] = true;
   }
-
   for (let i = 0; i < nums1.length; i++) {
     const curr = nums1[i];
-
     if (nums2Obj[curr] && !occured[curr]) {
       occured[curr] = true;
-
       result.push(nums1[i]);
     }
   }
@@ -28,14 +39,16 @@ var intersection = function (nums1, nums2) {
   return result;
 };
 
-// 方法二：排序 + 双指针
+// 方法三：排序 + 双指针
 var intersection = function (nums1, nums2) {
   nums1.sort((a, b) => a - b);
   nums2.sort((a, b) => a - b);
 
   let len1 = nums1.length;
   let len2 = nums2.length;
-  let l1, l2, result = [];
+  let l1,
+    l2,
+    result = [];
   l1 = l2 = 0;
 
   // 上一次出现相等的数字
