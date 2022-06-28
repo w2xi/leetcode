84748f59e4f0f8e03275fcd2d605629bd9324549
w2xi <43wangxi@gmail.com>
2022-06-28 22:56:06 +0800

    update 225. 用队列实现栈

--- algorithms/implement-stack-using-queues.js
+++ algorithms/implement-stack-using-queues.js
@@ -1,10 +1,13 @@
+// 方法一: 两个队列
+// 思路:
+// 使用一个备份队列
 var MyStack = function () {
   this.queue = [];
   // 用于备份
   this.backup = [];
 };
 
-/** 
+/**
  * @param {number} x
  * @return {void}
  */
@@ -24,7 +27,7 @@ MyStack.prototype.pop = function () {
   while (this.backup.length > 0) {
     this.queue.push(this.backup.shift());
   }
-  
+
   return this.queue.shift();
 };
 
@@ -34,7 +37,67 @@ MyStack.prototype.pop = function () {
 MyStack.prototype.top = function () {
   const result = this.pop();
   this.queue.push(result);
-  
+
+  return result;
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function () {
+  return this.queue.length === 0;
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
+
+
+// 方法二: 一个队列
+// 思路:
+// pop 时将除最后一个元素之外的全部弹出并依次 push 到队列中
+// 此时队列的第一个元素就是我们需要的
+
+var MyStack = function () {
+  this.queue = [];
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function (x) {
+  this.queue.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function () {
+  // pop
+  // [1, 2]
+  // =>
+  // [2, 1]
+  // =>
+  // [1]
+  let size = this.queue.length;
+  while (size-- > 1) {
+    this.queue.push(this.queue.shift());
+  }
+  return this.queue.shift();
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function () {
+  const result = this.pop();
+  this.queue.push(result);
   return result;
 };
 
