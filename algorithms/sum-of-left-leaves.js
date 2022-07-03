84856b686d0105042c1933dc3bec60366cf638ed
w2xi <43wangxi@gmail.com>
2022-07-03 22:09:27 +0800

    Update 404. 左叶子之和 前序遍历迭代

--- algorithms/sum-of-left-leaves.js
+++ algorithms/sum-of-left-leaves.js
@@ -7,13 +7,12 @@
  * }
  */
 /**
- * 左叶子之和
  * @param {TreeNode} root
  * @return {number}
  */
 var sumOfLeftLeaves = function (root) {
+  // 前序遍历 递归
   let sum = 0;
-
   const preOrderTraverse = (node) => {
     if (node) {
       // 判断是否是左叶子节点
@@ -24,8 +23,24 @@ var sumOfLeftLeaves = function (root) {
       preOrderTraverse(node.right);
     }
   };
-
   preOrderTraverse(root);
-
   return sum;
+
+  // 前序遍历 迭代
+  // return preOrder(root);
 };
+
+function preOrder(root) {
+  const stack = [root];
+  let sum = 0;
+  while (stack.length) {
+    const node = stack.pop();
+    // left leaf node
+    if (node.left && !node.left.left && !node.left.right) {
+      sum += node.left.val;
+    }
+    node.right && stack.push(node.right);
+    node.left && stack.push(node.left);
+  }
+  return sum;
+}
