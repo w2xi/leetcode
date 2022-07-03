0e7dadc3b0c5012a8c291f6fb83a4c7d3bab5a19
w2xi <43wangxi@gmail.com>
2022-07-03 11:24:30 +0800

    222. 完全二叉树的节点个数 DFS BFS 解法

--- algorithms/count-complete-tree-nodes.js
+++ algorithms/count-complete-tree-nodes.js
@@ -11,16 +11,34 @@
  * @return {number}
  */
 var countNodes = function (root) {
-  // 1. 递归遍历 O(n)
+  // 1. 后序遍历
+  if (!root) return 0;
+
+  const leftNum = countNodes(root.left);    // 左边节点的数量
+  const rightNum = countNodes(root.right);  // 右边节点的数量
+  const treeNum = 1 + leftNum + rightNum;   // 以中间节点为根节点的数量 (1 表示中间节点的数量)
+
+  return treeNum;
+
+  // 2. 层序遍历
+  // return bfs(root);
+};
+
+function bfs(root) {
+  if (!root) return 0;
+  const queue = [root];
   let count = 0;
-  const preOrderTraverse = (node) => {
-    if (node) {
+
+  while (queue.length) {
+    let size = queue.length;
+    while (size--) {
+      // 记录节点数
       count++;
-      preOrderTraverse(node.left);
-      preOrderTraverse(node.right);
+      const node = queue.shift();
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
     }
-  };
-  preOrderTraverse(root);
+  }
 
   return count;
-};
+}
