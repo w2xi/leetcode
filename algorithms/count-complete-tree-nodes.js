b31e96edd2958f7b7e8954e9d970d53cf4206720
w2xi <43wangxi@gmail.com>
2022-07-03 11:44:52 +0800

    Update 222. 完全二叉树的节点个数 后序遍历 (叉树)

--- algorithms/count-complete-tree-nodes.js
+++ algorithms/count-complete-tree-nodes.js
@@ -11,17 +11,20 @@
  * @return {number}
  */
 var countNodes = function (root) {
-  // 1. 后序遍历
-  if (!root) return 0;
+  // 1. 后序遍历 O(n)
+  // if (!root) return 0;
 
-  const leftNum = countNodes(root.left);    // 左边节点的数量
-  const rightNum = countNodes(root.right);  // 右边节点的数量
-  const treeNum = 1 + leftNum + rightNum;   // 以中间节点为根节点的数量 (1 表示中间节点的数量)
+  // const leftNum = countNodes(root.left);    // 左边节点的数量
+  // const rightNum = countNodes(root.right);  // 右边节点的数量
+  // const treeNum = 1 + leftNum + rightNum;   // 以中间节点为根节点的数量 (1 表示中间节点的数量)
 
-  return treeNum;
+  // return treeNum;
 
-  // 2. 层序遍历
+  // 2. 层序遍历 O(n)
   // return bfs(root);
+
+  // 3. 后序遍历 (完全二叉树) O(log n * log n)
+  return postOrder(root);
 };
 
 function bfs(root) {
@@ -41,4 +44,30 @@ function bfs(root) {
   }
 
   return count;
+}
+
+// O(log n * log n)
+function postOrder(root) {
+  if (!root) return 0;
+  let left = root.left;
+  let right = root.right;
+  let leftHeight = 0, rightHeight = 0;
+
+  while (left) {                    // 计算左子树左侧节点深度
+    leftHeight++;
+    left = left.left;
+  }
+  while (right) {                   // 计算右子树右侧节点深度
+    rightHeight++;
+    right = right.right;
+  }
+
+  if (leftHeight === rightHeight) { // 满二叉树节点数量
+    return (2 << leftHeight) - 1;
+  }
+
+  const leftNum = postOrder(root.left);
+  const rightNum = postOrder(root.right);
+
+  return 1 + leftNum + rightNum;
 }
