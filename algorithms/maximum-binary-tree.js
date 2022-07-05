bf5c291a1bd0b4c2597158781b23fad6a053ef30
w2xi <43wangxi@gmail.com>
2022-07-05 22:21:53 +0800

    Update 654. 最大二叉树 优化: 不分割数组,而是通过下标索引在原数组上操作

--- algorithms/maximum-binary-tree.js
+++ algorithms/maximum-binary-tree.js
@@ -12,6 +12,8 @@
  */
 var constructMaximumBinaryTree = function (nums) {
   // 分而治之
+
+  // 需要分割数组,产生了额外的内存开销
   if (nums.length === 0) return null;
   let maxIndex = findMaxValueIndex(nums);
   let rootValue = nums[maxIndex];
@@ -23,11 +25,35 @@ var constructMaximumBinaryTree = function (nums) {
   node.right = constructMaximumBinaryTree(right);
 
   return node;
-};
 
+  // 优化: 不分割数组,而是通过下标索引在原数组上操作
+  // return helper(nums, 0, nums.length);
+};
 function findMaxValueIndex(array) {
   let maxIndex = 0;
   for (let i = 1; i < array.length; i++) {
+    if (array[maxIndex] < array[i]) {
+      maxIndex = i;
+    }
+  }
+  return maxIndex;
+}
+
+// 优化: 不分割数组,而是通过下标索引在原数组上操作
+function helper(nums, left, right) {
+  if (left >= right) return null;
+  let maxIndex = findMaxIndex(nums, left, right);
+  let rootValue = nums[maxIndex];
+
+  const root = new TreeNode(rootValue);
+  root.left = helper(nums, left, maxIndex);
+  root.right = helper(nums, maxIndex + 1, right);
+
+  return root;
+}
+function findMaxIndex(array, start, end) {
+  let maxIndex = start;
+  for (let i = start + 1; i < end; i++) {
     if (array[maxIndex] < array[i]) {
       maxIndex = i;
     }
