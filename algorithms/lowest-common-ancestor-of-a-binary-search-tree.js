7898cf5c5246e62373045a7c83b0834ce6b18a4f
w2xi <43wangxi@gmail.com>
2022-07-09 12:23:29 +0800

    235. 二叉搜索树的最近公共祖先

---
+++ algorithms/lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  let low = p.val;
+  let high = q.val;
+  if (low > high) {
+    const temp = low;
+    low = high;
+    high = temp;
+  }
+  // 利用二叉搜索树的特性
+  // 只需要 root.val 在 [low, high] 区间 就说明 当前节点就是要找的 最近公共最近祖先
+  const dfs = (root) => {
+    if (!root) return null;
+    if (root.val > high) {          // 大于区间的最大值
+      return dfs(root.left);
+    } else if (root.val < low) {    // 小于区间的最小值
+      return dfs(root.right);
+    } else {                        // 在 [low, high] 之中
+      return root;
+    }
+  }
+  return dfs(root);
+};
