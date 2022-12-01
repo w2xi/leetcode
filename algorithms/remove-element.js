17f22d580db6301cf8eb956da29869cc62de6983
w2xi <43wangxi@gmail.com>
2022-12-01 22:45:42 +0800

    style: format code indent

--- algorithms/remove-element.js
+++ algorithms/remove-element.js
@@ -3,14 +3,14 @@
  * @param {number} val
  * @return {number}
  */
- var removeElement = function(nums, val) {
+var removeElement = function (nums, val) {
   // 双指针 (快慢指针)
   let slowIndex = 0;
 
   for (let fastIndex = 0; fastIndex < nums.length; fastIndex++) {
-      if (val !== nums[fastIndex]) {
-          nums[slowIndex++] = nums[fastIndex];
-      }
+    if (val !== nums[fastIndex]) {
+      nums[slowIndex++] = nums[fastIndex];
+    }
   }
 
   return slowIndex;
