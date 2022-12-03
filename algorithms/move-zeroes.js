7a3dc6e36355ac5c33e2631339b182cbc194d528
w2xi <43wangxi@gmail.com>
2022-12-03 10:27:17 +0800

    283. 移动零 (二刷)

--- algorithms/move-zeroes.js
+++ algorithms/move-zeroes.js
@@ -3,17 +3,31 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-  let index = 0;
+  // 双指针 (二刷的写法)
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== 0) {
-      nums[index] = nums[i];
-      index++;
+  let slowIndex = 0;
+
+  for (let fastIndex = 1; fastIndex < nums.length; fastIndex++) {
+    if (nums[slowIndex] === 0) {
+      if (nums[fastIndex] !== 0) {
+        swap(nums, slowIndex++, fastIndex);
+      }
+    } else {
+      slowIndex++;
     }
   }
-  for (let i = index; i < nums.length; i++) {
-    nums[i] = 0;
-  }
+
+  // let index = 0;
+
+  // for (let i = 0; i < nums.length; i++) {
+  //   if (nums[i] !== 0) {
+  //     nums[index] = nums[i];
+  //     index++;
+  //   }
+  // }
+  // for (let i = index; i < nums.length; i++) {
+  //   nums[i] = 0;
+  // }
 
   // 双指针
 
