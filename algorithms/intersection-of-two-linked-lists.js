b91f539541d415326bef22ed285b1f593e612790
w2xi <43wangxi@gmail.com>
2022-12-09 21:10:01 +0800

    Update 160. 相交链表

--- algorithms/intersection-of-two-linked-lists.js
+++ algorithms/intersection-of-two-linked-lists.js
@@ -13,17 +13,44 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function (headA, headB) {
+  // reference: https://programmercarl.com/%E9%9D%A2%E8%AF%95%E9%A2%9802.07.%E9%93%BE%E8%A1%A8%E7%9B%B8%E4%BA%A4.html#javascript
   if (!headA || !headB) {
     return null;
   }
+  const getLength = (head) => {
+    let count = 0;
+    let curr = head;
+    while (curr) {
+      count++;
+      curr = curr.next;
+    }
+    return count;
+  };
+  let aLen = getLength(headA);
+  let bLen = getLength(headB);
+  let diff = Math.abs(aLen - bLen);
 
-  let currA = headA;
-  let currB = headB;
-
-  while (currA !== currB) {
-    currA = currA ? currA.next : headB;
-    currB = currB ? currB.next : headA;
+  if (aLen < bLen) {
+    [headA, headB] = [headB, headA];
+    [aLen, bLen] = [bLen, aLen];
   }
+  while (diff--) {
+    headA = headA.next;
+  }
+  while (headA !== headB) {
+    headA = headA.next;
+    headB = headB.next;
+  }
+  return headA;
 
-  return currA;
+  // if (!headA || !headB) {
+  //   return null;
+  // }
+  // let currA = headA;
+  // let currB = headB;
+  // while (currA !== currB) {
+  //   currA = currA ? currA.next : headB;
+  //   currB = currB ? currB.next : headA;
+  // }
+  // return currA;
 };
