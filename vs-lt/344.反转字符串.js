0a43709adf21ad97b8d406ffa64f4326684452db
w2xi <43wangxi@gmail.com>
2022-12-10 09:22:52 +0800

    344. 反转字符串 (二刷)

--- vs-lt/344.反转字符串.js
+++ vs-lt/344.反转字符串.js
@@ -1,11 +1,15 @@
-// 方法一：双指针
+/*
+ * @lc app=leetcode.cn id=344 lang=javascript
+ *
+ * [344] 反转字符串
+ */
 
+// @lc code=start
 /**
- * 反转字符串
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
- var reverseString = function(s) {
+var reverseString = function(s) {
   let left = 0;
   let right = s.length - 1;
 
@@ -15,3 +19,5 @@
     right--;
   }
 };
+// @lc code=end
+
