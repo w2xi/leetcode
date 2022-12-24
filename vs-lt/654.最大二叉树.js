a08a5cccb3ec4e7d9ac5145c3e1f9f1572e8ede7
w2xi <43wangxi@gmail.com>
2022-12-24 11:55:41 +0800

    Update

--- vs-lt/654.最大二叉树.js
+++ vs-lt/654.最大二叉树.js
@@ -21,44 +21,25 @@
   // 分而治之
   // 需要分割数组, 会产生了额外的内存开销
 
-  // if (nums.length === 0) return null;
+  if (nums.length === 0) return null;
 
-  // let maxIndex = findMaxValueIndex(nums);
-  // let rootValue = nums[maxIndex];
-  // let left = nums.slice(0, maxIndex);
-  // let right = nums.slice(maxIndex + 1);
+  let maxIndex = findMaxValueIndex(nums);
+  let rootValue = nums[maxIndex];
+  let left = nums.slice(0, maxIndex);
+  let right = nums.slice(maxIndex + 1);
 
-  // const node = new TreeNode(rootValue);
-  // node.left = constructMaximumBinaryTree(left);
-  // node.right = constructMaximumBinaryTree(right);
+  const node = new TreeNode(rootValue);
+  node.left = constructMaximumBinaryTree(left);
+  node.right = constructMaximumBinaryTree(right);
 
-  // return node;
+  return node;
 
   // 优化: 不分割数组,而是通过下标索引在原数组上操作
-  return constructMaximumBinaryTree2(nums, 0, nums.length);
+  // return constructMaximumBinaryTree2(nums, 0, nums.length);
 };
 
-function findMaxValueIndex(array, start = 0, end = array.length) {
-  let maxIndex = start;
-  for (let i = start + 1; i < end; i++) {
-    if (array[maxIndex] < array[i]) {
-      maxIndex = i;
-    }
-  }
-  return maxIndex;
-}
-
+// 优化: 不分割数组,而是通过下标索引在原数组上操作
 function constructMaximumBinaryTree2(nums) {
-  // const findMaxValueIndex = (array, start, end) => {
-  //   let maxIndex = start;
-  //   for (let i = start + 1; i < end; i++) {
-  //     if (array[maxIndex] < array[i]) {
-  //       maxIndex = i;
-  //     }
-  //   }
-  //   return maxIndex;
-  // }
-  // 优化: 不分割数组,而是通过下标索引在原数组上操作
   const helper = (nums, start, end) => {
     // [3,2,1,6,0,5]
     if (start === end) return null;
@@ -77,6 +58,16 @@ function constructMaximumBinaryTree2(nums) {
   }
 
   return helper(nums, 0, nums.length);
+}
+
+function findMaxValueIndex(array, start = 0, end = array.length) {
+  let maxIndex = start;
+  for (let i = start + 1; i < end; i++) {
+    if (array[maxIndex] < array[i]) {
+      maxIndex = i;
+    }
+  }
+  return maxIndex;
 }
 // @lc code=end
 
