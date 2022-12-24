c76a0d640d09b44dde12a6cc11113f43c3547b82
w2xi <43wangxi@gmail.com>
2022-12-24 17:18:46 +0800

    Update

--- vs-lt/617.合并二叉树.js
+++ vs-lt/617.合并二叉树.js
@@ -21,9 +21,8 @@
 var mergeTrees = function(root1, root2) {
   if (!root2) return root1;
   if (!root1) return root2;
-  if (root1 && root2) {
-    root1.val += root2.val;
-  }
+  root1.val += root2.val;
+
   root1.left = mergeTrees(root1.left, root2.left);
   root1.right = mergeTrees(root1.right, root2.right);
 
