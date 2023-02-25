7639cec5afb56159e1f7019daff2922e0f57389a
w2xi <43wangxi@gmail.com>
2023-02-25 19:46:46 +0800

    Update 70. 爬楼梯

--- algorithms/climbing-stairs.js
+++ algorithms/climbing-stairs.js
@@ -1,41 +1,19 @@
 /**
- * 爬楼梯
+ * 70. 爬楼梯
  * @param {number} n
  * @return {number}
  */
 var climbStairs = function (n) {
-  // 1. DP
-
   // 1 2 3 5 8
-  // 递推公式
-  // f(x) = f(x-2) + f(x-1)
+  // dp(i) = dp(i - 1) + dp(i - 2)
 
-  if (n === 1) return 1;
-  if (n === 2) return 2;
-
-  let p = 1,
-    q = 2,
-    temp;
+  if (n <= 2) return n;
+  const dp = [1, 2];
 
   for (let i = 3; i <= n; i++) {
-    temp = p;
-    p = q;
-    q = temp + q;
+    const sum = dp[0] + dp[1];
+    dp[0] = dp[1];
+    dp[1] = sum;
   }
-
-  return q;
-
-  // 2. 递归 memo
-
-  // const memo = []
-  // const helper = (num) => {
-  //   if (num <= 2) return num;
-  //   if (memo[num] != null) {
-  //     return memo[num];
-  //   } else {
-  //     memo[num] = helper(num - 1) + helper(num - 2);
-  //     return memo[num];
-  //   }
-  // }
-  // return helper(n);
+  return dp[1];
 };
